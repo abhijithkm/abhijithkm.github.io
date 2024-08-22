8e05528b133ccead78a090d691a7a20da1b9aaba
Abhijith KM <abhijith.kumblasseriyil@micrologic.net>
2024-08-22 13:34:18 +0530

    Configure React app for deployment to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { Home } from '@mui/icons-material';
-import { Paper } from '@mui/material';
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import ThemeSwitcher from './components/ThemeSwitcher';
 
 function App() {
   return (
-    <Paper>
-      <ThemeSwitcher />
-      <Home />
-    </Paper>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
-import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeContextProvider } from './ThemeContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <CssBaseline />
-      <App />
-    </ThemeContextProvider>
+    <App />
   </React.StrictMode>
 );
 
